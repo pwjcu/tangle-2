d646da04a4a25f24ea53cdfc78cfd4a968682014
pwjcu <hwaranguk@gmail.com>
2026-01-13 22:03:23 +0900

    최신 로그인 모듈로 교체

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,17 +1,36 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
 
   if (code) {
     const cookieStore = cookies()
-    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-    await supabase.auth.exchangeCodeForSession(code)
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options })
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.set({ name, value: '', ...options })
+          },
+        },
+      }
+    )
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return NextResponse.redirect(`${origin}`)
+    }
   }
 
-  // 로그인 끝나면 메인으로 튕겨주기
-  return NextResponse.redirect(requestUrl.origin)
+  // 로그인 실패 시 메인으로 이동
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
